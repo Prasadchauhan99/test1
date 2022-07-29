13b69b8e6edab45d4a752a6366897b85fc94ab11
prasad chauhan <67370566+Prasadchauhan99@users.noreply.github.com>
2022-07-29 07:01:41 +0530

    Add files via upload

---
+++ src/demo/com/hello/Demo1.java
@@ -0,0 +1,16 @@
+package demo.com.hello;
+
+public class Demo1 {
+
+	public static void main(String[] args) 
+	{
+		
+		Demo1 d = new Demo1();
+	}
+		Demo1()
+		{
+			System.out.println("hello world");
+		}
+	
+	}
+	
